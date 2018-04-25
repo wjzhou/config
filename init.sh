3e4ea05fdef858d12210f4ba30dce33907c65c7d
Wujun Zhou <wujun@boxed.com>
2018-04-25 16:32:22 -0400

    add mac os init

--- init.sh
+++ init.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-if [ -e ~/.config/Code/User ] ; then
+
+case "$OSTYPE" in
+  darwin*)          path="$HOME/Library/Application Support/Code/User" ;; 
+  linux*)           path="$HOME/.config/Code/User" ;;
+  msys*|cygwin*)    path="$APPDATA/Code/User" ;;
+  *)        echo "unknown OS: $OSTYPE" ;;
+esac
+
+if [ -e "$path" ] ; then
     echo "vscode User folder exists, please delete and try again"
-    echo "rm -r ~/.config/Code/User"
+    echo "rm -r '$path'"
 else
-    mkdir -p ~/.config/Code
-    ln -s ~/config/Code/User ~/.config/Code/User
+    parent=$(dirname "$path")
+    mkdir -p "$parent"
+    ln -s ~/config/Code/User "$path"
 fi
